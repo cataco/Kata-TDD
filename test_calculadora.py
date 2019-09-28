1f0d794df84a071c3fe9c244c125bbe9cd3a720a
Catalina <cata.cordoba8@gmail.com>
2019-09-28 18:55:31 -0500

    Test cadena uno - rojo

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,3 +6,6 @@ from Calculadora import Calculadora
 class TestCalculadora(TestCase):
     def testSumar(self):
         self.assertEqual(Calculadora().sumar(""), 0, "empty")
+
+    def testSumarUno(self):
+        self.assertEqual(Calculadora().sumar("1"), 1, "empty")
